723d4f3fb5060b39b10cbe0cbb008a5f703c5bd0
Mikey <miketran.pl@gmail.com>
2023-03-23 23:34:18 +0000

    home - mobile background fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -35,11 +35,9 @@ export default {
 <style lang="scss" scoped>
 .main {
   background-image: url(../assets/images/main_pic1.jpg);
-  background-repeat: no-repeat;
   max-width: 100%;
   width: 100%;
   height: 100vh;
-  background-attachment: fixed;
   background-position: top;
   background-repeat: no-repeat;
   background-size: cover;
