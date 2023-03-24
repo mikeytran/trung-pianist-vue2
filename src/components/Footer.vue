f0574dce1b0e6d1db8f211c34aa4fd2defc87e05
Mikey <miketran.pl@gmail.com>
2023-03-24 01:04:00 +0000

    footer - added copyright

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="main">
     <div class="main__container">
-      <p>
-        Website by:
-        <a href="https://www.linkedin.com/in/mike-tran-uk/" target="_blank"
-          >Mike Tran</a
-        >
-      </p>
+      <div class="left-block">
+        <p>&#169; 2023 Nguyen Viet Trung</p>
+      </div>
+      <div class="right-block">
+        <p>
+          Website by:
+          <a href="https://www.linkedin.com/in/mike-tran-uk/" target="_blank"
+            >Mike Tran</a
+          >
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -23,10 +28,32 @@ export default {
   width: 100%;
   margin: 0 auto;
   background-color: #000000;
-  padding: 20px 20px 10px;
+  padding-top: 30px;
+  padding-bottom: 10px;
   &__container {
-    p {
-      text-align: center;
+    max-width: 1760px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    .left-block,
+    .right-block {
+      width: 50%;
+    }
+    .right-block {
+      p {
+        text-align: right;
+      }
+    }
+  }
+}
+
+@media (max-width: 600px) {
+  .main {
+    &__container {
+      p {
+        font-size: 12px;
+      }
     }
   }
 }
