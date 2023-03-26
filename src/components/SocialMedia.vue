a9730810cc30af7761694e86772c10ed59912444
Mikey <miketran.pl@gmail.com>
2023-03-26 19:14:29 +0100

    social-media - menago details added

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="contact" class="main">
-    <h2>CONNECT WITH ME</h2>
+    <h2>FOLLOW ME</h2>
     <div class="main__container">
       <a href="https://www.instagram.com/trung_pianist/" target="_blank">
         <img src="@/assets/images/insta.png" alt="Insta" />
@@ -20,6 +20,14 @@
       <a href="mailto:viettrung96@gmail.com" target="_blank">
         <img src="@/assets/images/gmail.png" alt="Gmail" />
       </a>
+    </div>
+    <h2>BUSINESS INQUIRIES</h2>
+    <div class="menager__container">
+      <p>Zofia Rogala</p>
+      <a href="mailto:rogala.zofia@gmail.com" target="_blank"
+        >rogala.zofia@gmail.com</a
+      >
+      <a href="tel:+48 601 867 724">+48 601 867 724</a>
     </div>
   </div>
 </template>
@@ -44,6 +52,26 @@ export default {
 .main {
   width: 100%;
   background-color: #000000;
+  h2 {
+    &:nth-of-type(2) {
+      margin-top: 60px;
+      margin-bottom: 30px;
+    }
+  }
+  .menager__container {
+    max-width: 400px;
+    margin: 0 auto;
+    p {
+      text-align: center;
+      font-size: 19px;
+      margin-bottom: 0px;
+    }
+    a {
+      display: block;
+      text-align: center;
+      font-size: 19px;
+    }
+  }
   &__container {
     width: 100%;
     max-width: 400px;
