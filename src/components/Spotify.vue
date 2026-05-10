e24f56666227c1af86cba452f25bbc0dfe9a78d5
Mikey <mikey@Mikeys-MacBook-Pro.local>
2026-05-10 23:19:27 +0100

    adding apple music

--- src/components/Spotify.vue
+++ src/components/Spotify.vue
@@ -12,6 +12,20 @@
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
       ></iframe>
+      <iframe
+        allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
+        frameborder="0"
+        height="450"
+        style="
+          width: 100%;
+          height: 352;
+          overflow: hidden;
+          border-radius: 10px;
+          padding-top: 20px;
+        "
+        sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+        src="https://embed.music.apple.com/us/album/frederic-chopin-viet-trung-nguyen-plays-chopin/1595610106"
+      ></iframe>
     </div>
   </div>
 </template>
